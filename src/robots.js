817953b000668da1a3bd6485fe03969f6a152354
Curtis Dunne <curtdunne@gmail.com>
2018-05-25 08:37:36 -0600

    Init repo

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Burtron Raoolala',
+		username: 'Burtron',
+		email: 'burtron@burtronraoolala.com'
+	},
+	{
+		id: 2,
+		name: 'JoAllen Raoolala',
+		username: 'Jojo',
+		email: 'jojo@burtronraoolala.com'
+	},
+	{
+		id: 3,
+		name: 'Mooie Machismo',
+		username: 'Mooie',
+		email: 'mooie@burtronraoolala.com'
+	},
+	{
+		id: 4,
+		name: 'Mauie Machismo',
+		username: 'Mauie',
+		email: 'mauie@burtronraoolala.com'
+	},
+	{
+		id: 5,
+		name: 'Macho Machismo',
+		username: 'Macho',
+		email: 'macho@burtronraoolala.com'
+	},
+	{
+		id: 6,
+		name: 'Stretch Machismo',
+		username: 'Stretch',
+		email: 'stretch@burtronraoolala.com'
+	},
+		{
+		id: 7,
+		name: 'Lefty Machismo',
+		username: 'Lefty',
+		email: 'lefty@burtronraoolala.com'
+	},
+	{
+		id: 8,
+		name: 'Lauren Sanchez',
+		username: 'SoftBoobies',
+		email: 'hotlorin@burtronraoolala.com'
+	},
+			{
+		id: 9,
+		name: 'ChickWith A GreatAss',
+		username: 'MyNextWife',
+		email: 'greatass@starnursery.com'
+	},
+];
